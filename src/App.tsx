486f24c48f6f414d8822f9c63ed20ec568d734da
Quentin Barroin <quentin.barroin@gmail.com>
2020-08-21 18:06:43 +0200

    feat:add spinner

--- src/App.tsx
+++ src/App.tsx
@@ -64,6 +64,34 @@ interface IIngredient {
   strIngredient1: string;
 }
 
+function Spinner() {
+  return (
+    <svg
+      width="38"
+      height="38"
+      viewBox="0 0 38 38"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="#000"
+    >
+      <g fill="none" fill-rule="evenodd">
+        <g transform="translate(1 1)" stroke-width="2">
+          <circle stroke-opacity=".5" cx="18" cy="18" r="18" />
+          <path d="M36 18c0-9.94-8.06-18-18-18">
+            <animateTransform
+              attributeName="transform"
+              type="rotate"
+              from="0 18 18"
+              to="360 18 18"
+              dur="1s"
+              repeatCount="indefinite"
+            />
+          </path>
+        </g>
+      </g>
+    </svg>
+  );
+}
+
 // composant principal
 function App() {
   const [selectedList, setselectedList] = useState(todolist);
@@ -139,11 +167,22 @@ function App() {
         <h4>Liste des ingrédients récupérés depuis une API</h4>
         <label htmlFor="searchIngredient">Rechercher un ingrédient</label>
         <input type="text" onChange={searchIngredient} />
-        <ul className="list">
-          {ingredientsList.map((ingredient: any) => (
-            <li key={ingredient.strIngredient1}>{ingredient.strIngredient1}</li>
-          ))}
-        </ul>
+
+        {ingredientsList.length === 0 && (
+          <>
+            <p>CONTENU EN COURS DE CHARGEMENT</p>
+            <Spinner />
+          </>
+        )}
+        {ingredientsList.length > 0 && (
+          <ul className="list">
+            {ingredientsList.map((ingredient: any) => (
+              <li key={ingredient.strIngredient1}>
+                {ingredient.strIngredient1}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </Fragment>
   );
